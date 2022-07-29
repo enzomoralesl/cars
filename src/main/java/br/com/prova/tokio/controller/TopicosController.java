0910d5b42eaaca713a2bb39fd471215d98c22b14
enzomoralesl <enzo_moralesl@yahoo.com.br>
2022-07-29 12:54:40 -0300

    get listar

--- src/main/java/br/com/prova/tokio/controller/TopicosController.java
+++ src/main/java/br/com/prova/tokio/controller/TopicosController.java
@@ -2,21 +2,31 @@ package br.com.prova.tokio.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.prova.tokio.controller.dto.TopicoDto;
 import br.com.prova.tokio.model.Topico;
+import br.com.prova.tokio.repository.TopicoRepository;
 
 
 @RestController
 public class TopicosController {
 	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
 	public List<TopicoDto> listar(String titulo) {
-
-			List<Topico> topicos = null;
+		if (titulo == null) {
+			List<Topico> topicos = topicoRepository.findAll();
 			return TopicoDto.converter(topicos);
+		}
+		else {
+			List<Topico> topicos = topicoRepository.findByTitulo(titulo);
+			return TopicoDto.converter(topicos);
+		}
 	}
 	
 	
