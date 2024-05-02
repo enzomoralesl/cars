e4c3da4016702218bf96eaef3dc5d8098aa95194
enzomoralesl <enzo_moralesl@yahoo.com.br>
2024-05-02 01:50:16 -0300

    Request Json internal Validation completed

--- src/main/java/br/com/cars/dto/CarRequest.java
+++ src/main/java/br/com/cars/dto/CarRequest.java
@@ -10,6 +10,7 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -20,7 +21,7 @@ public class CarRequest {
 	private String brand;
 	@NotBlank(message = "The model is required.") @Length(min = 1)
 	private String model;
-	@Valid @JsonProperty("carUser")
+	@Valid @JsonProperty("carUser") @NotNull(message = "The car user is required.")
 	private CarUserRequest carUser;
 	private Status status = Status.NO_STATE;
 }
